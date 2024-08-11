4a38cb5eb06abe9fe66284185a8425a19c924cc8
khangronky <nguyengiakhang646@gmail.com>
2024-08-11 23:49:55 +0700

    refactor: Update customers page layout and functionality

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,9 +1,31 @@
-import { Metadata } from 'next';
+import Search from "@/app/ui/search";
+import Table from "@/app/ui/customers/table";
+import { lusitana } from "@/app/ui/fonts";
+import { TableRowSkeleton } from "@/app/ui/skeletons";
+import { Suspense } from "react";
+import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: 'Customers',
+  title: "Customers",
 };
 
-export default function Page() {
-    return <p>Customers Page</p>;
+export default function Page({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+  };
+}) {
+  const query = searchParams?.query || "";
+  return (
+    <div className="w-full">
+      <h1 className={`${lusitana.className} mb-8 text-xl md:text-2xl`}>
+        Customers
+      </h1>
+      <Search placeholder="Search customers..." />
+      <Suspense fallback={<TableRowSkeleton />}>
+        <Table query={query} />
+      </Suspense>
+    </div>
+  );
 }
